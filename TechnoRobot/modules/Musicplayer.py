fe4f2e1fa2f15abf112593f41fc06a693067189b
Team-techno <103729598+Team-techno@users.noreply.github.com>
2022-05-18 13:38:34 +0530

    Create Musicplayer.py

---
+++ TechnoRobot/modules/Musicplayer.py
@@ -0,0 +1,23 @@
+__mod_name__ = "Alexiaa Player"
+
+__help__ = """
+=>> *Song Playing* 🎧 
+❍ /play <song name> - play song you requested
+❍ /vplay <song name or video name> -play song or video you requested
+❍ /playlist - Show now playing list
+❍ /current - Show now playing
+❍ /song <song name> - download songs you want quickly
+❍ /search <query> - search videos on youtube with details
+❍ /play <song name> - download videos you want quickly
+=>> *Admins only*
+❍ /player - open music player settings panel
+❍ /pause - pause song play
+❍ /resume - resume song play
+❍ /skip - play next song
+❍ /end - stop music play
+
+❍ /admincache - Refresh admin list
+=>>   *Use*
+1) Add [Techno](https://t.me/Techno_x_robot) in your group to admin with all permission
+2) Add Assistant I'd to your group [click here](https://t.me/Technomusicassistant)
+"""
