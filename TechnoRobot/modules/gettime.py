ff771d2b6b67ecce19e9ebec6f1fc349d1f715af
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 03:26:02 +0530

    Update gettime.py

--- TechnoRobot/modules/gettime.py
+++ TechnoRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from TechnoRobot import TIME_API_KEY, dispatcher
-from TechnoRobot.modules.disable import DisableAbleCommandHandler
+from JarvisRobot import TIME_API_KEY, dispatcher
+from JarvisRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
