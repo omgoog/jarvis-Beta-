74f4307ed12d199a9ab7328d5636412cfd339822
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 03:01:04 +0530

    Update admin.py

--- TechnoRobot/modules/admin.py
+++ TechnoRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from TechnoRobot import DRAGONS, dispatcher
-from TechnoRobot.modules.disable import DisableAbleCommandHandler
-from TechnoRobot.modules.helper_funcs.chat_status import (
+from JarvisRobot import DRAGONS, dispatcher
+from JarvisRobot.modules.disable import DisableAbleCommandHandler
+from JarvisRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -15,19 +15,19 @@ from TechnoRobot.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from TechnoRobot.helper_extra.admin_rights import (
+from JarvisRobot.helper_extra.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
 )
 
-from TechnoRobot.modules.helper_funcs.extraction import (
+from JarvisRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from TechnoRobot.modules.log_channel import loggable
-from TechnoRobot.modules.helper_funcs.alternate import send_message
-from TechnoRobot.modules.helper_funcs.alternate import typing_action
+from JarvisRobot.modules.log_channel import loggable
+from JarvisRobot.modules.helper_funcs.alternate import send_message
+from JarvisRobot.modules.helper_funcs.alternate import typing_action
 
 
 @run_async
