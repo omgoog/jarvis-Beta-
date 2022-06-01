9b2a81be7c8f153f6260dfc801efdd294ae022d9
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 03:18:59 +0530

    Update muting.py

--- TechnoRobot/modules/muting.py
+++ TechnoRobot/modules/muting.py
@@ -1,20 +1,20 @@
 import html
 from typing import Optional
 
-from TechnoRobot import LOGGER, TIGERS, dispatcher
-from TechnoRobot.modules.helper_funcs.chat_status import (
+from JarvisRobot import LOGGER, TIGERS, dispatcher
+from JarvisRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
     is_user_admin,
     user_admin,
 )
-from TechnoRobot.modules.helper_funcs.extraction import (
+from JarvisRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from TechnoRobot.modules.helper_funcs.string_handling import extract_time
-from TechnoRobot.modules.log_channel import loggable
+from JarvisRobot.modules.helper_funcs.string_handling import extract_time
+from JarvisRobot.modules.log_channel import loggable
 from telegram import Bot, Chat, ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
