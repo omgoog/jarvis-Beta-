83c4fabe2a47e1176f09977b39110f05876ad7ec
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 02:46:42 +0530

    Update __forcesubs.py

--- TechnoRobot/modules/__forcesubs.py
+++ TechnoRobot/modules/__forcesubs.py
@@ -12,9 +12,9 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
-from TechnoRobot import DRAGONS as SUDO_USERS
-from TechnoRobot import pbot
-from TechnoRobot.modules.sql_extended import forceSubscribe_sql as sql
+from JarvisRobot import DRAGONS as SUDO_USERS
+from JarvisRobot import pbot
+from JarvisRobot.modules.sql_extended import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
 
