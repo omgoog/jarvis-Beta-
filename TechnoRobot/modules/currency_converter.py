31a44e9d084e80f68ba449b68d3b135895832eff
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 03:13:29 +0530

    Update currency_converter.py

--- TechnoRobot/modules/currency_converter.py
+++ TechnoRobot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from TechnoRobot import CASH_API_KEY, dispatcher
+from JarvisRobot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
