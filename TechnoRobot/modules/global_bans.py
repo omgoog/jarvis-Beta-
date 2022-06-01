7189675153951f3c1300d06227e3d42e211e51c8
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 03:26:48 +0530

    Update global_bans.py

--- TechnoRobot/modules/global_bans.py
+++ TechnoRobot/modules/global_bans.py
@@ -14,9 +14,9 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-import TechnoRobot.modules.sql.global_bans_sql as sql
-from TechnoRobot.modules.sql.users_sql import get_user_com_chats
-from TechnoRobot import (
+import JarvisRobot.modules.sql.global_bans_sql as sql
+from JarvisRobot.modules.sql.users_sql import get_user_com_chats
+from JarvisRobot import (
     DEV_USERS,
     EVENT_LOGS,
     OWNER_ID,
@@ -30,16 +30,16 @@ from TechnoRobot import (
     sw,
     dispatcher,
 )
-from TechnoRobot.modules.helper_funcs.chat_status import (
+from JarvisRobot.modules.helper_funcs.chat_status import (
     is_user_admin,
     support_plus,
     user_admin,
 )
-from TechnoRobot.modules.helper_funcs.extraction import (
+from JarvisRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from TechnoRobot.modules.helper_funcs.misc import send_to_list
+from JarvisRobot.modules.helper_funcs.misc import send_to_list
 
 GBAN_ENFORCE_GROUP = 6
 
