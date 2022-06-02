04dee4edc9e73b597ce76e378ea4f9d488c43723
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 11:05:41 +0530

    Update mongo.py

--- TechnoRobot/mongo.py
+++ TechnoRobot/mongo.py
@@ -2,24 +2,24 @@
 import asyncio
 import sys
 
-from TechnoRobot import log
+from JarvisRobot import log
 from motor import motor_asyncio
-from TechnoRobot import MONGO_DB_URI 
+from JarvisRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from TechnoRobot.conf import get_int_key, get_str_key
+from JarvisRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "TechnoRobot"
+MONGO_DB = "JarvisRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["TechnoRobot"]
+db = client["JarvisRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
