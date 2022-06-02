fd9079ea6d0b40751d1fc26158d7e5404019c856
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:38:24 +0530

    Update connection_sql.py

--- TechnoRobot/modules/sql/connection_sql.py
+++ TechnoRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from TechnoRobot.modules.sql import SESSION, BASE
+from JarvisRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
