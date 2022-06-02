c8b99e9b76857c2918eb73139f8c1bd010bed99a
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:58:57 +0530

    Update chat_status.py

--- TechnoRobot/modules/helper_funcs/chat_status.py
+++ TechnoRobot/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from TechnoRobot import (
+from JarvisRobot import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from TechnoRobot.modules import connection
+from JarvisRobot.modules import connection
 
 connected = connection.connected
