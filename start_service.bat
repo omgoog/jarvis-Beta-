286d953af478b50baa2a42f3eb1415f43c2bbd25
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 11:18:44 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it
-net stop TechnoRobot
-net start TechnoRobot
+net stop JarvisRobot
+net start JarvisRobot
