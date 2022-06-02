bb7cc85a9a397027cc78cb4279a2c4288a847684
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 11:14:19 +0530

    Update pluginshelper.py

--- TechnoRobot/pyrogramee/pluginshelper.py
+++ TechnoRobot/pyrogramee/pluginshelper.py
@@ -12,8 +12,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from TechnoRobot  import OWNER_ID, SUPPORT_CHAT
-from TechnoRobot import pbot
+from JarvisRobot  import OWNER_ID, SUPPORT_CHAT
+from JarvisRobot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
