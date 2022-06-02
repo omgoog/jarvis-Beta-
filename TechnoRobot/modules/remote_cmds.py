86ece5ea623ccc8f56e93309a03fd93f12c34962
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:17:12 +0530

    Update remote_cmds.py

--- TechnoRobot/modules/remote_cmds.py
+++ TechnoRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from TechnoRobot import dispatcher
-from TechnoRobot.modules.helper_funcs.chat_status import (
+from JarvisRobot import dispatcher
+from JarvisRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from TechnoRobot.modules.helper_funcs.extraction import extract_user_and_text
-from TechnoRobot.modules.helper_funcs.filters import CustomFilters
+from JarvisRobot.modules.helper_funcs.extraction import extract_user_and_text
+from JarvisRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
