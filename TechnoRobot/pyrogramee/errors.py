36c7299b93165b67ed359e7df143adaa01c4c6a4
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 11:14:49 +0530

    Update errors.py

--- TechnoRobot/pyrogramee/errors.py
+++ TechnoRobot/pyrogramee/errors.py
@@ -2,7 +2,7 @@
 import sys
 import traceback
 from functools import wraps
-from TechnoRobot import pbot, SUPPORT_CHAT
+from JarvisRobot import pbot, SUPPORT_CHAT
 
 def split_limits(text):
     if len(text) < 2048:
