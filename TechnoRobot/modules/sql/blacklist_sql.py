78caa95beb0851be98d88f0c0f207b37387c3cb0
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:33:26 +0530

    Update blacklist_sql.py

--- TechnoRobot/modules/sql/blacklist_sql.py
+++ TechnoRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, BigInteger
 
-from TechnoRobot.modules.sql import SESSION, BASE
+from JarvisRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
