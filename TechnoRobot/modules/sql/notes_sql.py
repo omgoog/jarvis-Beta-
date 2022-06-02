b504244dc1abe8b17c15692a80fac14be1184864
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:42:00 +0530

    Update notes_sql.py

--- TechnoRobot/modules/sql/notes_sql.py
+++ TechnoRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from TechnoRobot.modules.helper_funcs.msg_types import Types
-from TechnoRobot.modules.sql import BASE, SESSION
+from JarvisRobot.modules.helper_funcs.msg_types import Types
+from JarvisRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 
