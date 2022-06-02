d8ed0e736d4b50b21c7cc06ec97f909349bff224
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 10:41:06 +0530

    Update locks_sql.py

--- TechnoRobot/modules/sql/locks_sql.py
+++ TechnoRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from TechnoRobot.modules.sql import SESSION, BASE
+from JarvisRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
