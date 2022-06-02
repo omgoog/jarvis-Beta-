ee09c6a9e4656255d5d8d01fdb11c583532e37a5
omgoog <89709117+omgoog@users.noreply.github.com>
2022-06-02 11:01:15 +0530

    Update msg_types.py

--- TechnoRobot/modules/helper_funcs/msg_types.py
+++ TechnoRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from TechnoRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from JarvisRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
